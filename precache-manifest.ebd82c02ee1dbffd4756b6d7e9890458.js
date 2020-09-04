9514941d2d09006d719acb46193a9f431707365c
Shevchenko Elena <elena.kadru@gmail.com>
2020-09-04 17:47:50 +0300

    Updates

---
+++ precache-manifest.ebd82c02ee1dbffd4756b6d7e9890458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6e8e2e0210b963539dd66a5f202f97",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "e7aaf70c3acaf20ce6b8",
+    "url": "/goit-react-hw-02-feedback/static/css/main.cb9a5be1.chunk.css"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7aaf70c3acaf20ce6b8",
+    "url": "/goit-react-hw-02-feedback/static/js/main.13e0ee95.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
