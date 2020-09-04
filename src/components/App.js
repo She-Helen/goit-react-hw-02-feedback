6512cd3d869b134342cf375cef8ff6ae6cca92f6
Shevchenko Elena <elena.kadru@gmail.com>
2020-09-04 15:54:31 +0300

    added files

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import { Feedback } from "./feedback/Feedback";
+
+export function App() {
+  return (
+    <Fragment>
+      <Feedback />
+    </Fragment>
+  );
+}
